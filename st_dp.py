7c2328d6f3f587ccc4ad20ab5786e5259cf1fed4
Fromadhana <f.romadhana@gmail.com>
2023-03-04 18:10:47 +0700

    v3

--- st_dp.py
+++ st_dp.py
@@ -85,64 +85,83 @@ if authentication_status ==True:
 
   
   #display streamlit form
-  def run_first_tab():
-    with tab1:
-      st.header("Sisa Stock Sebelumnya")
+  def main_page():
+      st.header("Sisa Stock Kemarin ⏪️")
       with st.form(key= "so_before", clear_on_submit=True):
-        so_sm = st.selectbox('**Sesi Siang/Malam?**', ('SIANG', 'MALAM'))
-        
-        #sisa stock sebelumnya
-        st.caption('**Sisa stock sebelumnya** ⏪️' )
+        #sisa stock kemarin
         col1, col2, col3 = st.columns(3)
         st.write('''<style>
-          [data-testid="column"] {
-              width: calc(33.3333% - 1rem) !important;
-              flex: 1 1 calc(33.3333% - 1rem) !important;
-              min-width: calc(33% - 1rem) !important;
-          }
-          </style>''', unsafe_allow_html=True)
+            [data-testid="column"] {
+                width: calc(33.3333% - 1rem) !important;
+                flex: 1 1 calc(33.3333% - 1rem) !important;
+                min-width: calc(33% - 1rem) !important;
+            }
+            </style>''', unsafe_allow_html=True)
         with col1:
-          pss1 = st.selectbox("Produk", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'))
-          pss2 = st.selectbox("p2", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pss3 = st.selectbox("p3", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pss4 = st.selectbox("p4", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pss5 = st.selectbox("p5", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pss6 = st.selectbox("p6", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")      
+            pss1 = st.selectbox("Produk", ('','LTK', 'BSK', 'BRP', 'BRS', 'PI6', 'PI8'))
+            pss2 = st.selectbox("p2", ('','LTK', 'BSK', 'BRP', 'BRS', 'PI6', 'PI8'), label_visibility="collapsed")
+            pss3 = st.selectbox("p3", ('','LTK', 'BSK', 'BRP', 'BRS', 'PI6', 'PI8'), label_visibility="collapsed")
+            pss4 = st.selectbox("p4", ('','LTK', 'BSK', 'BRP', 'BRS', 'PI6', 'PI8'), label_visibility="collapsed")
+            pss5 = st.selectbox("p5", ('','LTK', 'BSK', 'BRP', 'BRS', 'PI6', 'PI8'), label_visibility="collapsed")
+            pss6 = st.selectbox("p6", ('','LTK', 'BSK', 'BRP', 'BRS', 'PI6', 'PI8'), label_visibility="collapsed")      
 
         with col2:
-          nss1 = st.text_input('Jumlah')
-          nss2 = st.text_input('n2', label_visibility="collapsed")
-          nss3 = st.text_input('n3', label_visibility="collapsed")
-          nss4 = st.text_input('n4', label_visibility="collapsed")
-          nss5 = st.text_input('n5', label_visibility="collapsed")
-          nss6 = st.text_input('n6', label_visibility="collapsed")
-        
+            nss1 = st.number_input('Jumlah', step=1)
+            nss2 = st.number_input('n2', label_visibility="collapsed", step=1)
+            nss3 = st.number_input('n3', label_visibility="collapsed", step=1)
+            nss4 = st.number_input('n4', label_visibility="collapsed", step=1)
+            nss5 = st.number_input('n5', label_visibility="collapsed", step=1)
+            nss6 = st.number_input('n6', label_visibility="collapsed", step=1)
+          
         with col3:
-          tss1 = st.text_input('Expired')
-          tss2 = st.text_input('t2', label_visibility="collapsed")
-          tss3 = st.text_input('t3', label_visibility="collapsed")
-          tss4 = st.text_input('t4', label_visibility="collapsed")
-          tss5 = st.text_input('t5', label_visibility="collapsed")
-          tss6 = st.text_input('t6', label_visibility="collapsed")
+            tss1 = st.text_input('Expired')
+            tss2 = st.text_input('t2', label_visibility="collapsed")
+            tss3 = st.text_input('t3', label_visibility="collapsed")
+            tss4 = st.text_input('t4', label_visibility="collapsed")
+            tss5 = st.text_input('t5', label_visibility="collapsed")
+            tss6 = st.text_input('t6', label_visibility="collapsed")
 
         #submit button
-        submitted = st.form_submit_button(label="**SUBMIT**", use_container_width=True)
+        submitted = st.form_submit_button(label="**SUBMIT**", use_container_width=True, type="primary")
         if submitted:
+          #sum all number
+          sum_nss = nss1 + nss2 + nss3 + nss4 + nss5 + nss6
+          #create dataframe
+          df1 = pd.DataFrame({"Produk": [pss1, pss2, pss3, pss4, pss5, pss6], 
+                              "Jumlah Stock": [nss1, nss2, nss3, nss4, nss5, nss6], 
+                              "Tanggal Expired": [tss1, tss2, tss3, tss4, tss5, tss6]})
+          #CSS to inject contained in a string
+          hide_table_row_index = """
+                <style>
+                thead tr th:first-child {display:none}
+                tbody th {display:none}
+                </style>
+                """
+          #inject CSS with markdown
+          hide = st.markdown(hide_table_row_index, unsafe_allow_html=True)
+          container = st.container()
+          container.write(":orange[Jumlah Sisa Stock Kemarin]")
+          st.table(df1)
           #show related information
+          st.markdown("""
+                      <style>
+                      [data-testid=column]:nth-of-type(4) [data-testid=stVerticalBlock]{
+                          gap: 0rem;
+                      }
+                      </style>
+                      """,unsafe_allow_html=True)
+          st.write("Drop Point: {}".format(name))
           st.write("Tanggal/Jam : {}".format(timenow))
-          st.write("Stock Opname : {}".format(so_sm))
+          st.write("Total Sisa Stock Kemarin : {}".format(sum_nss))
 
         else:
           st.warning('Isi sesuai jumlah stock yang ada di drop point', icon="⚠️")
           st.stop()
-    return None
-  
-  def run_second_tab():
-    with tab2:
+    
+  def page2():
       st.header("Stock Masuk Hari Ini")
       with st.form(key= "so_today", clear_on_submit=True):
-        so_sm = st.selectbox('**Sesi Siang/Malam?**', ('SIANG', 'MALAM'))
-        
+       
         #stock masuk hari ini
         st.caption('**Stock Masuk Hari Ini** ⤵️' )
         col6, col7, col8 = st.columns(3)
@@ -154,93 +173,43 @@ if authentication_status ==True:
           }
           </style>''', unsafe_allow_html=True)
         with col6:
-          pmh1 = st.selectbox("Produk", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'))
-          pmh2 = st.selectbox("p2", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pmh3 = st.selectbox("p3", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pmh4 = st.selectbox("p4", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pmh5 = st.selectbox("p5", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pmh6 = st.selectbox("p6", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")      
+            pmh1 = st.selectbox("Produk", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'))
+            pmh2 = st.selectbox("p2", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
+            pmh3 = st.selectbox("p3", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
+            pmh4 = st.selectbox("p4", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
+            pmh5 = st.selectbox("p5", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
+            pmh6 = st.selectbox("p6", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")      
 
         with col7:
-          nmh1 = st.text_input('Jumlah')
-          nmh2 = st.text_input('n2', label_visibility="collapsed")
-          nmh3 = st.text_input('n3', label_visibility="collapsed")
-          nmh4 = st.text_input('n4', label_visibility="collapsed")
-          nmh5 = st.text_input('n5', label_visibility="collapsed")
-          nmh6 = st.text_input('n6', label_visibility="collapsed")
+            nmh1 = st.text_input('Jumlah')
+            nmh2 = st.text_input('n2', label_visibility="collapsed")
+            nmh3 = st.text_input('n3', label_visibility="collapsed")
+            nmh4 = st.text_input('n4', label_visibility="collapsed")
+            nmh5 = st.text_input('n5', label_visibility="collapsed")
+            nmh6 = st.text_input('n6', label_visibility="collapsed")
         
         with col8:
-          tmh1 = st.text_input('Expired')
-          tmh2 = st.text_input('t2', label_visibility="collapsed")
-          tmh3 = st.text_input('t3', label_visibility="collapsed")
-          tmh4 = st.text_input('t4', label_visibility="collapsed")
-          tmh5 = st.text_input('t5', label_visibility="collapsed")
-          tmh6 = st.text_input('t6', label_visibility="collapsed")
+            tmh1 = st.text_input('Expired')
+            tmh2 = st.text_input('t2', label_visibility="collapsed")
+            tmh3 = st.text_input('t3', label_visibility="collapsed")
+            tmh4 = st.text_input('t4', label_visibility="collapsed")
+            tmh5 = st.text_input('t5', label_visibility="collapsed")
+            tmh6 = st.text_input('t6', label_visibility="collapsed")
 
         #submit button
         submitted = st.form_submit_button(label="**SUBMIT**", use_container_width=True)
         if submitted:
           #show related information
           st.write("Tanggal/Jam : {}".format(timenow))
-          st.write("Stock Opname : {}".format(so_sm))
 
         else:
           st.warning('Isi sesuai jumlah stock yang ada di drop point', icon="⚠️")
           st.stop()
-    return None
-  
-  def run_third_tab():
-    with tab3: 
-      st.header("Penjualan Hari Ini")
-      with st.form(key= "so_endday", clear_on_submit=True):
-        so_sm = st.selectbox('**Sesi Siang/Malam?**', ('SIANG', 'MALAM'))
-        
-        #Penjualan Hari Ini
-        st.caption('**Penjualan Hari Ini** ⤴️' )
-        col9, col10, col11 = st.columns(3)
-        st.write('''<style>
-          [data-testid="column"] {
-              width: calc(33.3333% - 1rem) !important;
-              flex: 1 1 calc(33.3333% - 1rem) !important;
-              min-width: calc(33% - 1rem) !important;
-          }
-          </style>''', unsafe_allow_html=True)
-        with col9:
-          pph1 = st.selectbox("Produk", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'))
-          pph2 = st.selectbox("p2", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pph3 = st.selectbox("p3", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pph4 = st.selectbox("p4", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pph5 = st.selectbox("p5", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")
-          pph6 = st.selectbox("p6", ('','LTK', 'BSK', 'BPK', 'BSK', 'PI6', 'PI8'), label_visibility="collapsed")      
+      
+  page_names_to_funcs = {
+     "Sisa Stock Sebelumnya": main_page,
+     "Stock Masuk Hari ini": page2,
+  }
 
-        with col10:
-          nph1 = st.text_input('Jumlah')
-          nph2 = st.text_input('n2', label_visibility="collapsed")
-          nph3 = st.text_input('n3', label_visibility="collapsed")
-          nph4 = st.text_input('n4', label_visibility="collapsed")
-          nph5 = st.text_input('n5', label_visibility="collapsed")
-          nph6 = st.text_input('n6', label_visibility="collapsed")
-        
-        with col11:
-          tph1 = st.text_input('Expired')
-          tph2 = st.text_input('t2', label_visibility="collapsed")
-          tph3 = st.text_input('t3', label_visibility="collapsed")
-          tph4 = st.text_input('t4', label_visibility="collapsed")
-          tph5 = st.text_input('t5', label_visibility="collapsed")
-          tph6 = st.text_input('t6', label_visibility="collapsed")
-
-        #submit button
-        submitted = st.form_submit_button(label="**SUBMIT**", use_container_width=True)
-        if submitted:
-          #show related information
-          st.write("Tanggal/Jam : {}".format(timenow))
-          st.write("Stock Opname : {}".format(so_sm))
-
-        else:
-          st.warning('Isi sesuai jumlah stock yang ada di drop point', icon="⚠️")
-          st.stop()
-    return None
-  tab1, tab2, tab3 = st.tabs(["Sisa Stock Sebelumnya", "Stock Masuk Hari Ini", "Penjualan Hari Ini"])
-  run_first_tab()
-  run_second_tab()
-  run_third_tab()
+  selected_page = st.sidebar.selectbox("Pilih Halaman", page_names_to_funcs.keys())
+  page_names_to_funcs[selected_page]()
