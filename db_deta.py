c812801d4f261129fc04033a086448a49b73c023
Fromadhana <f.romadhana@gmail.com>
2023-03-05 00:25:57 +0700

    v3

--- db_deta.py
+++ db_deta.py
@@ -17,3 +17,7 @@ def insert_user(username, name, password):
     """Returns the user on a succesful user creation, otherwise raises an error"""
     return db.put({"key": username, "name": name, "password": password})
 
+def fetch_all_user():
+    """Returns a dict of all users"""
+    res = db.fetch()
+    return res.items
